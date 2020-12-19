c9bdb04780814fc67c7838e367c3f1c20552f243
Richard Pompeo <pompeorichard@gmail.com>
2020-12-19 19:10:01 -0300

    New help command

--- src/commands/miscellaneous/Help.js
+++ src/commands/miscellaneous/Help.js
@@ -3,14 +3,11 @@ const { MessageEmbed } = require("discord.js");
 module.exports.run = async (bot, message) => {
   const embed = new MessageEmbed()
     .setColor(message.guild.me.roles.highest.color)
-    .setTitle(`Grove Ajuda`)
-    .setDescription(
-      `
-      Olá ${message.author},
-      para ver minha lista de comandos acesse https://grovebot.vercel.app/index.html#navbar,
-      você também pode pegar em meu site o convite do meu servidor de suporte e o link para me adicionar!
-      Para acessar meu site acesse https://grovebot.vercel.app
-      `
+    .setAuthor(message.guild.me.user.username, message.guild.me.user.avatarURL())
+    .addFields(
+      { name: 'Sobre', value: 'Sou um bot Brasileiro com suporte a diversas plataformas de Streaming!', inline: false },
+      { name: 'WebSite', value: 'Em meu site você pode saber mais sobre mim! [Clique aqui](https://grovebot.tk)', inline: false },
+      { name: 'Comandos', value: 'Para ver minha lista de comandos, [clique aqui!](https://grovebot.tk/commands)', inline: false }
     )
   message.channel.send(embed)
 }
